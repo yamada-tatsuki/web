92064b13d08e954359c706cd82cb5702961a3c4d
yamada_tatsuki <ta-yamada@virtualex.co.jp>
2020-04-22 18:15:03 +0900

    サーブレット編集１

--- src/app/HobbyServlet.java
+++ src/app/HobbyServlet.java
@@ -1,6 +1,7 @@
 package app;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -13,6 +14,8 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @WebServlet("/api/hobby")
 public class HobbyServlet extends HttpServlet {
@@ -27,21 +30,21 @@ public class HobbyServlet extends HttpServlet {
 		// TODO 必須機能「趣味参照機能」
 		String syainid = request.getParameter("syainid");
 
-		try {
-
-		    // JDBCドライバのロード
-		    Class.forName("oracle.jdbc.driver.OracleDriver");
-
-		} catch (ClassNotFoundException e) {
-		    // ドライバが設定されていない場合はエラーになります
-		    throw new RuntimeException(String.format("JDBCドライバのロードに失敗しました。詳細:[%s]", e.getMessage()), e);
-		}
 
 		String url = "jdbc:oracle:thin:@localhost:1521:XE";
 		String user = "wt2";
 		String pass = "wt2";
 
-		String sql = "select  \n" +
+		try (
+				// データベースへ接続します
+				Connection con = DriverManager.getConnection("jdbc:log4jdbc:oracle:thin:@localhost:1521:XE","wt2","wt2");
+
+				// SQLの命令文を実行するための準備をおこないます
+				Statement stmt = con.createStatement();
+
+				// SQLの命令文を実行し、その結果をResultSet型のrsに代入します
+				ResultSet rs1 = stmt.executeQuery(
+				"select  \n" +
 				"msh.SYAINID, \n" +
 				"mh.HOBBY_ID, \n" +
 				"mh.HOBBY_NAME, \n" +
@@ -54,33 +57,31 @@ public class HobbyServlet extends HttpServlet {
 				"MS_CATEGORY mc \n" +
 				" \n" +
 				"where 1=1 \n" +
+				"and SYANID = '"+ syainid + "' \n "+
 				"and msh.HOBBY_ID = mh.HOBBY_ID \n" +
 				"and mh.CATEGORY_ID = mc.CATEGORY_ID \n"
-				;
+				);
 
 		List<Hobby>hobbyList = new ArrayList<>();
-
-		;try (
-				// データベースへ接続します
-				Connection con = DriverManager.getConnection(url, user, pass);
-
-				// SQLの命令文を実行するための準備をおこないます
-				Statement stmt = con.createStatement();
-
-				// SQLの命令文を実行し、その結果をResultSet型のrsに代入します
-				ResultSet rs1 = stmt.executeQuery(sql);) {
-			// SQL実行後の処理内容
 
 			// SQL実行結果を保持している変数rsから商品情報を取得
 			// rs.nextは取得した商品情報表に次の行があるとき、trueになります
 			// 次の行がないときはfalseになります
 			while(rs1.next()) {
+				Hobby hobby = new Hobby();
 
-				item.setHobby(rs1.getString("mh.HOBBY_NAME")); // Item型の変数itemに商品コードをセット
-				item.setHobbyCategory(rs1.getString("mc.CATEGORY_NAME"));// Item型の変数itemに商品名をセット
+				hobby.setHobby(rs1.getString("mh.HOBBY_NAME")); // Item型の変数itemに商品コードをセット
+				hobby.setHobbyCategory(rs1.getString("mc.CATEGORY_NAME"));// Item型の変数itemに商品名をセット
 
-
+				hobbyList.add(hobby);
 			}
+
+			// アクセスした人に応答するためのJSONを用意する
+			PrintWriter pw = response.getWriter();
+
+			// JSONで出力する
+			pw.append(new ObjectMapper().writeValueAsString(hobbyList));
+
 		} catch (Exception e) {
 			throw new RuntimeException(String.format("検索処理の実施中にエラーが発生しました。詳細：[%s]", e.getMessage()), e);
 		}
